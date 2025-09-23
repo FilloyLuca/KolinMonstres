8f8edf274f9231254a76e946b6fcf0b76acef537
FilloyLuca <filloylucapro@gmail.com>
2025-09-23 08:47:22 +0200

    Debut CombatMonstre

---
+++ src/main/kotlin/jeu/CombatMonstre.kt
@@ -0,0 +1,10 @@
+package org.example.jeu
+
+import org.example.monstre.IndividuMonstre
+
+class CombatMonstre (
+    monstreJoueur : IndividuMonstre,
+    monstreSauvage : IndividuMonstre,
+    var round : Int = 1
+){
+}
