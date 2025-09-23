6a7fb90423b031b2d4f1203e38b359f812ab3ebf
FilloyLuca <filloylucapro@gmail.com>
2025-09-23 08:54:19 +0200

    fun gameOver()

--- src/main/kotlin/jeu/CombatMonstre.kt
+++ src/main/kotlin/jeu/CombatMonstre.kt
@@ -1,5 +1,7 @@
 package org.example.jeu
 
+import org.example.dresseur.Entraineur
+import org.example.joueur
 import org.example.monstre.IndividuMonstre
 
 class CombatMonstre (
@@ -7,4 +9,19 @@ class CombatMonstre (
     monstreSauvage : IndividuMonstre,
     var round : Int = 1
 ){
+    /**
+     * Vérifie si le joueur a perdu le combat.
+     *
+     * Condition de défaite :
+     * - Aucun monstre de l'équipe du joueur n'a de PV > 0.
+     *
+     * @return `true` si le joueur a perdu, sinon `false`.
+     */
+    fun gameOver(): Boolean{
+        for (monstre in joueur.equipeMonstre){
+            if (monstre.pv > 0) return false
+        }
+        println("Game Over !")
+        return true
+    }
 }
